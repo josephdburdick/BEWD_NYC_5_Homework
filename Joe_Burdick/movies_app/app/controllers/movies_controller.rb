d1ddbd9398a34dca200a67b4c65e167e42f337d1
Joe Burdick <josephdburdick@gmail.com>
2013-11-29 17:41:52 -0500

    Adding movie files

--- Joe_Burdick/movies_app/app/controllers/movies_controller.rb
+++ Joe_Burdick/movies_app/app/controllers/movies_controller.rb
@@ -3,4 +3,17 @@ class MoviesController < ApplicationController
   	@movies = Movie.all
   end
 
+  def edit
+  	movie = Movie.find params[:id]
+  	render :show
+  end
+
+  def create
+  	@movie = Movie.create link: params[:id]
+  	redirect_to new_movie_path(@movie)
+  end
+
+  def show
+  end
+
 end
