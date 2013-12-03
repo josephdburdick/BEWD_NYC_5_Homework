6d738c7dda11cc95377eb375f89892460b0bea0e
Joe Burdick <josephdburdick@gmail.com>
2013-12-02 21:15:26 -0500

    Added authentication

--- Joe_Burdick/movies_app/config/routes.rb
+++ Joe_Burdick/movies_app/config/routes.rb
@@ -1,7 +1,7 @@
 MoviesApp::Application.routes.draw do
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' } #override/change default paths (rake routes)
+
   get "search", to: 'search#index'
-  
-  
   root "movies#index"
   resources :movies
 
